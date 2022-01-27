c0c2fa6bc787df3ba86ca2e0ae762a171e5e089d
skmttkys <skmttkys1986@gmail.com>
2022-01-27 17:55:55 +0900

    鍵ファイルをigonore対象に追加、ルートindexが初期画面なので変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+
+  
   resources :testdbs
   # DBを表示する
   get "test" => "testdbs#index"
 
+  #localhost:3000が初期画面なので差し替える
+  get "/" => "home#index"
   # home/topをtopとして表示する
   get "top" => "home#top"
   get "about" => "home#about"
+  get "item" => "home#item"
 
   # hello,world!を表示する
   # root 'application#hello'
