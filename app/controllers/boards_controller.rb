cd51f2f8d3e99c00b829722c6baf4a0ae4cb38d2
Takashi Hamada <project.user@TakashinoMacBook-Pro.local>
2022-02-01 20:18:59 +0900

    update modify indents
    インデントの調整, footerのダミー追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,41 +1,41 @@
 class BoardsController < ApplicationController
-    def index
-        @boards = Board.all
-    end
+  def index
+    @boards = Board.all.order(id: 'asc')
+  end
 
-    # 新規作成
-    def new
-        @board = Board.new
-    end
+  # 新規作成
+  def new
+    @board = Board.new
+  end
 
-    def create
-        @board = Board.create(board_params)
-        redirect_to boards_path
-    end
+  def create
+    @board = Board.create(board_params)
+    redirect_to boards_path
+  end
 
-    # 編集
-    def edit
-        @board = Board.find(params[:id])
-    end
+  # 編集
+  def edit
+    @board = Board.find(params[:id])
+  end
 
-    # 更新
-    def update
-        @board = Board.find(params[:id])
-        @board.update(board_params)
-        redirect_to boards_path
-    end
+  # 更新
+  def update
+    @board = Board.find(params[:id])
+    @board.update(board_params)
+    redirect_to boards_path
+  end
 
-    # 削除
-    def destroy
-        @board = Board.find(params[:id])
-        @board.destroy
-        redirect_to boards_path
-    end
+  # 削除
+  def destroy
+    @board = Board.find(params[:id])
+    @board.destroy
+    redirect_to boards_path
+  end
 
-    # パラメータ定義
-    private
-        # permitで、フォームで設定している値を設定
-        def board_params
-            params.require(:board).permit(:name, :description)
-        end
+  # パラメータ定義
+  private
+    # permitで、フォームで設定している値を設定
+    def board_params
+      params.require(:board).permit(:name, :description)
+    end
 end
